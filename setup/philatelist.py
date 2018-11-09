7673bc09d383d41ae8fde1dae175e13e1fc80642
FernandoLVentura <44762230+FernandoLVentura@users.noreply.github.com>
2018-11-10 01:06:45 +0200

    Added correction for corrupt images
    Some images (less than 1%) appear to be damaged and caused the original script to crash. This fixes that and ensures a buffer of files to replace corrupt ones. Also cleaned up the comments a bit.

--- setup/philatelist.py
+++ setup/philatelist.py
@@ -1,5 +1,5 @@
 #
-#   Philatelist
+#   Philatelist (Stamp Collector)
 #   ______________________________________________________________
 #
 #   -Go through a catalogue and seek useful sources.
@@ -47,8 +47,9 @@ catFile = 'catalog_13jun05.bin'                     #File location of catalogue
 FIRST_dirs = '/data/users/deane/vla_first/data/'    #File location of FIRST directories
 album = 'stamps/'                                   #File location of stamps
 resultingCat = 'output/stampCat.csv'                #Catalogue of training sources
+errList = 'output/files_with_problems.txt'
 
-numStamps = 100000                                  #Maximum number of training stamps allowed
+numStamps = 50000                                   #Maximum number of training stamps allowed
 stampSize = 128                                     #Size of the stamp in pixels
 
 
@@ -84,8 +85,6 @@ DECdeg = np.array(DECdeg)
 #Get initial list of possible filenames
 print('Getting initial list of possible associated filenames...')
 
-#The below process should be handled more elegantly, as above.
-#Might rewrite this.
 filenames = []
 f = open(catFile,'r')
 for line in f:
@@ -101,12 +100,6 @@ filenames_subset = [filenames[i] for i in ind]
 subcat = np.array([RAdeg[ind],DECdeg[ind],Fpeak[ind],ind])
 RAdeg = RAdeg[ind]
 DECdeg = DECdeg[ind]
-
-#Remove the below after testing
-print('\nREQUIRED FILES:\n')
-for fn in range(len(filenames_subset)):
-    print(filenames_subset[fn])
-print('\nEND OF FILES\n')
 
 print('Ready to collect files...')
 
@@ -124,49 +117,53 @@ ra_foh = []
 dec_foh = []
 
 ns = 0
+
+haveFile = 0
+altFile = 0
+noFile = 0
+
+numFilesWithBuffer = 1.2*numStamps # Do this to account for corrupt files.
+
 for obj in range(len(ind)):
 
-    fileLoaded = False;
-    
-    print(filenames_subset[obj], end='')
-    print(': ', end='')
     
     #Check if it exists.
     try:
         if os.path.isfile(filenames_subset[obj]):
-            fileLoaded = True;
             
             files_on_hand.append(filenames_subset[obj])
             ra_foh.append(RAdeg[obj])
             dec_foh.append(DECdeg[obj])
             
-            ns = ns + 1
-            print('ON HAND.')
+            ns += 1
+            haveFile += 1
             #If it does not exist, seek an alternative:
         else:
             alternate = glob.glob(filenames_subset[obj][:-6] + '*.fits')
             if (len(alternate) > 0 and os.path.isfile(alternate[-1])):
-                fileLoaded = True;
                 
                 files_on_hand.append(alternate[-1])
                 ra_foh.append(RAdeg[obj])
                 dec_foh.append(DECdeg[obj])
             
-                print('ALTERNATE: ', end='')
-                print(alternate[-1])
-                ns = ns + 1
+                altFile += 1
+                ns += 1
             else:
-                print('NO SUITABLE FILE.')
+                noFile += 1
     except IOError:
         print(' FILE ACCESS ERROR: ' + filenames_subset[obj])
 	
-    if ( ns == numStamps ): break   #Stop when we reach the max required.
+    if ( ns == numFilesWithBuffer ): break   #Stop when we reach the max required.
     #If no suitable file was found or an error occurred, move to the next image.
 
 files_on_hand = np.array(files_on_hand)
 ra_foh = np.array(ra_foh)
 dec_foh = np.array(dec_foh)
 
+print('Of ' + str(ns+noFile) + " files processed: ")
+print('ON HAND: ' + str(haveFile) + "\t" + str(100.0*haveFile/(ns+noFile)) + '%')
+print('ALTERNATE: ' + str(altFile) + "\t" + str(100.0*altFile/(ns+noFile)) + '%')
+print('NONE: ' + str(noFile) + "\t" + str(100.0*noFile/(ns+noFile)) + '%')
     
     
 #
@@ -184,105 +181,118 @@ warnings.filterwarnings('ignore')
 #myHiPSfs = HiPSfs(hips_file, noCache=True)
 
 output = open(bin_file_path, 'wb')                  # output file opened for byte writing
-output.write(struct.pack('i', len(files_on_hand)))  # number of objects
+output.write(struct.pack('i', numStamps))           # number of objects
 output.write(struct.pack('i', 1))                   # number of channels
 output.write(struct.pack('i', stampSize) )          # width
 output.write(struct.pack('i', stampSize) )          # height
 
-#for b in range(len(sRA)):
-#    print(sRA[b], ', ', sDC[b])
 
 len_catalog = len(files_on_hand)
 count = 0
 info = []
+
+numErrs = 0
+errFiles = []
+
 print('Number of sources: ' + str(len_catalog))
 
 for i in range(len(files_on_hand)):
     
+    if count == numStamps: break #Stop when we have enough
+    
     # Notify when every N objects has been completed
-    if i % 1 == 0:
-        print(str(i) + '/' + str(len_catalog))
-    
+    if i % 1000 == 0:
+        print( "\n" + str(i) + '/' + str(len_catalog))
+        
     #Generate cutout
-    #print('Getting pixel coordinates: ')
-    ra, dec = (ra_foh[i], dec_foh[i]) #Coordinates of this source
     
-    #print(ra, ", ", dec)
     
-    fileData = fits.open(files_on_hand[i])
-    hdr = fileData[0].header
-    #dim = np.array(pixels.shape)
-    #print("Dimensions: ", dim)
+    try: 
+        ra, dec = (ra_foh[i], dec_foh[i]) #Coordinates of this source
     
-    hdr.remove('CRPIX3')
-    hdr.remove('CRVAL3')
-    hdr.remove('CDELT3')
-    hdr.remove('CTYPE3')
-    hdr.remove('CROTA3')
-    hdr.remove('NAXIS3')
+        fileData = fits.open(files_on_hand[i])
+        hdr = fileData[0].header
     
-    hdr.remove('CRPIX4')
-    hdr.remove('CRVAL4')
-    hdr.remove('CDELT4')
-    hdr.remove('CTYPE4')
-    hdr.remove('CROTA4')
-    hdr.remove('NAXIS4')
+	# These files have a third and fourth axis which must be removed for processing
+        hdr.remove('CRPIX3')
+        hdr.remove('CRVAL3')
+        hdr.remove('CDELT3')
+        hdr.remove('CTYPE3')
+        hdr.remove('CROTA3')
+        hdr.remove('NAXIS3')
     
-    hdr['NAXIS'] = 2
-    
-    #print(hdr['CRVAL1'], ", ", hdr['CRVAL2'])
-    
-    w = WCS( header = hdr ) #WCS object for this source
-    
-    x, y = w.all_world2pix(ra, dec, 1, adaptive=True) #Pixel coordinates of target
-    #print(str(x) + ', ' + str(y))
-    #print(files_on_hand[i])
-    
-    #print('Cutting stamp out.')
-    
-    position = (x, y)
-    imData = fileData[0].data[0][0] #Shape it correctly. Assumes 4D. Might rewrite to generalise.
-    cutout = Cutout2D(imData, position, size, mode='partial') #Anything past the border filled with NaN
-    
-    image = np.array(cutout.data) #Get the image data as a np array.
-    
-    empty = np.isnan(image) #Get all NaN pixels and replace with normalised noise.
-    image[empty] = np.random.normal(loc=np.mean(image[~empty]), scale=np.std(image[~empty]), size=image.shape)[empty] # replace missing values with noise  
-    image = image - np.min(image) #Set base intensity to 0
-    
-    # clip background
-    image = np.clip(image, clip_threshold*np.std(image), 1e10)
-    
-    # Normalize using minmax
-    mminterval = vis.MinMaxInterval()
-    image = mminterval(image)
-    
-    image.astype('f').tofile(output) #Write to the Binary file
-    
-    #Append the info to our catalogue
-    entry = []
-    entry.append(str(ra))
-    entry.append(str(dec))
-    entry.append(files_on_hand[i])
-    file = album + str(count) + '-' + str(int(round(ra))) + ':' + str(int(round(dec))) + '.stamp.fits'
-    entry.append(file)
-    info.append(entry)
-    
-    #Create a FITS image of the stamp
-    hdu = fits.PrimaryHDU()
-    hdu.data = image
-    hdu.header['CRVAL1'] = ra
-    hdu.header['CRVAL2'] = dec
-    
-    hdu.writeto(file, clobber=True)
-    
-    count += 1
-
+        hdr.remove('CRPIX4')
+        hdr.remove('CRVAL4')
+        hdr.remove('CDELT4')
+        hdr.remove('CTYPE4')
+        hdr.remove('CROTA4')
+        hdr.remove('NAXIS4')
+        
+        hdr['NAXIS'] = 2
+        
+        w = WCS( header = hdr ) #WCS object for this source
+        
+        x, y = w.all_world2pix(ra, dec, 1, adaptive=True) #Pixel coordinates of target
+        
+        position = (x, y)
+        imData = fileData[0].data[0][0] #Shape it correctly. Assumes 4D data.
+        cutout = Cutout2D(imData, position, size, mode='partial') #Anything past the border filled with NaN
+        
+        image = np.array(cutout.data) #Get the image data as a np array.
+        
+	#
+	# Following taken from sample script:
+	# ///////////////////////////////////
+	
+        empty = np.isnan(image) #Get all NaN pixels and replace with normalised noise.
+        image[empty] = np.random.normal(loc=np.mean(image[~empty]), scale=np.std(image[~empty]), size=image.shape)[empty] # replace missing values with noise  
+        image = image - np.min(image) #Set base intensity to 0
+        
+        # clip background
+        image = np.clip(image, clip_threshold*np.std(image), 1e10)
+        
+        # Normalize using minmax
+        mminterval = vis.MinMaxInterval()
+        image = mminterval(image)
+        
+        image.astype('f').tofile(output) #Write to the Binary file
+        
+	#
+	# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	#
+	
+        # Append the info to our catalogue
+        entry = []
+        entry.append(str(ra))
+        entry.append(str(dec))
+        entry.append(files_on_hand[i])
+        file = album + str(count) + '-' + str(int(round(ra))) + ':' + str(int(round(dec))) + '.stamp.fits'
+        entry.append(file)
+        info.append(entry)
+        
+        # Create a FITS image of the stamp
+        hdu = fits.PrimaryHDU()
+        hdu.data = image
+        hdu.header['CRVAL1'] = ra
+        hdu.header['CRVAL2'] = dec
+        
+	# Save the stamp for later reference
+        hdu.writeto(file, clobber=True)
+        
+        count += 1
+        
+    except:
+        numErrs += 1
+        errFiles.append(files_on_hand[i])
         
 output.close()
+
 print('Number of images in binary:', count, ' of ', len_catalog)
 info = np.array(info)
-print("Catalogue array: " + str( info.shape ))
+
+print("Number of files with errors: " + str(numErrs))
+errInfo = np.array(errFiles)
+np.savetxt(errList, errInfo, fmt="%s", delimiter='\n', newline='\n')
 
 np.savetxt(resultingCat, info, fmt="%s", delimiter=',', newline='\n', header='RA,DEC,Origin File,Stamp File', comments='# ')
 
