d082cbcfa29ade5bd963362f172b622efa237c53
FernandoLVentura <44762230+FernandoLVentura@users.noreply.github.com>
2018-12-21 14:12:11 +0200

    Update displayMaps.py

--- visualiseResults/displayMaps.py
+++ visualiseResults/displayMaps.py
@@ -1,1 +1,26 @@
 # Display the SOM or heatmaps using these functions.
+from heatmapVisualiser import *
+import os
+import glob
+import numpy as np
+
+def viewHM(path):
+	m = MAPVisualizer(path, path)
+	print(path)
+	m.readMap()
+	m.showMap()
+	raw_input("Press ENTER to continue.")
+	
+def viewMultipleHM(paths):
+	for p in paths:
+		viewHM(p)
+		
+def viewAllInFolder(path):
+	#os.chdir(path)
+	f = []
+	for e in glob.glob( path + "*.bin"): f.append(e)
+	f = np.array(f)
+	
+	f = f[np.argsort(f)]
+	
+	viewMultipleHM(f)
